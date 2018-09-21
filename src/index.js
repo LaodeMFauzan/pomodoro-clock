dd179fbd838486f0916a10517d723538a7ecc1f2
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2018-09-21 23:35:33 +0700

    add display component

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Button extends React.Component{
-	
+	render(){
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+}
+
+class Display extends React.Component{
+	render(){
+		return (
+			<div>
+				
+			</div>
+		)
+	}
 }
